ea1ea58279d6bc6f473c189c2f320ba2e813eceb
John Burnett <tuninator@gmail.com>
2017-10-07 14:31:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-init();
-
 function init() {
   // Write your JavaScript code inside the init() function
 
   let index = 0;
 
-//  document.body.addEventListener("keydown", function(e) ) {
+  document.body.addEventListener("keydown", function(e) ) {
 
   // get numerical value of key
   const key = parseInt(e.detail || e.which)
